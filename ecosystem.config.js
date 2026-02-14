22448e43c19b71a62d96d794a6e4c373aaa2ff5e
Pablo Jesús Calvente Ramírez <calventeramirez@outlook.com>
2026-02-14 21:31:10 +0100

    Agregar archivo de configuración de PM2 para la aplicación biblioteca

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'biblioteca',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
+    cwd: '/home/pi/Desktop/biblioteca/logFront',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: './logs/pm2-error.log',
+    out_file: './logs/pm2-out.log',
+    time: true
+  }]
+}
